566b0093f65a3d2d5795b17d8d5b0635b0189ec4
DanTheCoder <danielkaplan812@gmail.com>
2025-02-22 21:18:31 -0500

    fixed port issues

--- lend_backend/app.py
+++ lend_backend/app.py
@@ -33,4 +33,4 @@ def create_app():
 if __name__ == "__main__":
     flask_app = create_app()
     # The app will run on http://127.0.0.1:5000 by default
-    flask_app.run(host="0.0.0.0", port=8080)
+    flask_app.run(host="0.0.0.0", port=8081)
